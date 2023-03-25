0e1065dd7e3590dae8126f0830a6ac171e064dd8
akinyemi <hiibeekayvibe@gmail.com>
2023-03-25 22:24:59 +0100

    finished the header

--- public_html/templates/header.php
+++ public_html/templates/header.php
@@ -32,6 +32,8 @@ $filename = $row['filename'];
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 
+<!-- the body -->
+
 <body>
     <header>
         <div class="site-title">
