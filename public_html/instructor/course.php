267a08935e10fcdb6c96711f250fe969b1e23285
akinyemi <hiibeekayvibe@gmail.com>
2023-04-02 13:48:55 +0100

    the courses dashboard and the course page.

--- public_html/instructor/course.php
+++ public_html/instructor/course.php
@@ -5,6 +5,36 @@ include '../templates/header.php';
 <div class="main">
     <?php include '../templates/side-bar.php'; ?>
     <div class="main-body" id="content">
+
+        <!-- course page navigation -->
+        <!-- add course, grade, send private message -->
+        <div class="course-nav">
+            <a href="add-course.php">
+                <div class="course-nav-item">
+                    <span class="material-symbols-outlined">
+                        note_add
+                    </span>
+                    <p>Add Course</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="course-nav-item">
+                    <span class="material-symbols-outlined">
+                        assignment_turned_in
+                    </span>
+                    <p>Grade</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="course-nav-item">
+                    <span class="material-symbols-outlined">
+                        chat
+                    </span>
+                    <p>Send Private Message</p>
+                </div>
+            </a>
+        </div>
+
         <div class="course-overview">
             <!-- display all courses -->
 
@@ -33,6 +63,16 @@ include '../templates/header.php';
 
                     </tbody>
                 </table>
+            </div>
+            <!-- pagination -->
+            <div class="pagination">
+                <a href="#">&laquo;</a>
+                <a href="#">1</a>
+                <a href="#">2</a>
+                <a href="#">3</a>
+                <a href="#">4</a>
+                <a href="#">5</a>
+                <a href="#">&raquo;</a>
             </div>
         </div>
 
--- public_html/instructor/dash.php
+++ public_html/instructor/dash.php
@@ -4,8 +4,37 @@ include '../templates/header.php';
 ?>
 <div class="main">
     <?php include '../templates/side-bar.php'; ?>
-    <div class="main" id="content">
-        <div class="overview">
+    <div class="main-body" id="content">
+        <div class="main-body" id="content">
+
+            <!-- course page navigation -->
+            <!-- add course, grade, send private message -->
+            <div class="course-nav">
+                <a href="add-course.php">
+                    <div class="course-nav-item">
+                        <span class="material-symbols-outlined">
+                            note_add
+                        </span>
+                        <p>Add Course</p>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="course-nav-item">
+                        <span class="material-symbols-outlined">
+                            assignment_turned_in
+                        </span>
+                        <p>Grade</p>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="course-nav-item">
+                        <span class="material-symbols-outlined">
+                            chat
+                        </span>
+                        <p>Send Private Message</p>
+                    </div>
+                </a>
+            </div>
 
         </div>
     </div>
